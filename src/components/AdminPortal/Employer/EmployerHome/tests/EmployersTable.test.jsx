ad8fdf741ad7ca9357af60bf3df45e2c10841c07
Zivile Dauderiene <ziviledauderiene@coherentsolutions.com>
2022-11-09 12:41:46 +0200

    error handling test

--- src/components/AdminPortal/Employer/EmployerHome/tests/EmployersTable.test.jsx
+++ src/components/AdminPortal/Employer/EmployerHome/tests/EmployersTable.test.jsx
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import { endpoints } from 'consts';
+import server from 'mocks/server';
+import { rest } from 'msw';
 
-import { EmployersTable } from 'components';
-import { BrowserRouter } from 'react-router-dom';
+import { EmployerHome, EmployersTable } from 'components';
 import mockEmployersList from 'mocks/data/mockEmployersList';
+import { BrowserRouter } from 'react-router-dom';
 
 test('display list of employers from the server', async () => {
   render(
@@ -13,4 +16,24 @@ test('display list of employers from the server', async () => {
 
   const employerRows = await screen.findAllByRole('row');
   expect(employerRows).toHaveLength(4);
+});
+
+test('handles errors', async () => {
+  server.resetHandlers(
+    rest.get(
+      `${process.env.REACT_APP_API_URL}${endpoints.employers}`,
+      (req, res, ctx) => {
+        res(ctx.status(500));
+      }
+    )
+  );
+
+  render(
+    <BrowserRouter>
+      <EmployerHome />
+    </BrowserRouter>
+  );
+
+  const errorMessage = await screen.findByText(/Could not fetch employers/i);
+  expect(errorMessage).toBeInTheDocument();
 });

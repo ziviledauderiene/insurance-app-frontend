17f3f2ca46b2fbf51037b57b76a932fcd3dc2519
Zivile Dauderiene <ziviledauderiene@coherentsolutions.com>
2022-11-10 10:40:13 +0200

    override render method with wrapper

--- src/components/AdminPortal/Employer/EmployerHome/tests/EmployersTable.test.jsx
+++ src/components/AdminPortal/Employer/EmployerHome/tests/EmployersTable.test.jsx
@@ -1,18 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from 'test-utils/testing-library-utils';
 import { endpoints } from 'consts';
 import server from 'mocks/server';
 import { rest } from 'msw';
 
 import { EmployerHome, EmployersTable } from 'components';
 import mockEmployersList from 'mocks/data/mockEmployersList';
-import { BrowserRouter } from 'react-router-dom';
 
 test('display list of employers from the server', async () => {
-  render(
-    <BrowserRouter>
-      <EmployersTable employersList={mockEmployersList} />
-    </BrowserRouter>
-  );
+  render(<EmployersTable employersList={mockEmployersList} />);
 
   const employerRows = await screen.findAllByRole('row');
   expect(employerRows).toHaveLength(4);
@@ -22,17 +17,11 @@ test('handles errors', async () => {
   server.resetHandlers(
     rest.get(
       `${process.env.REACT_APP_API_URL}${endpoints.employers}`,
-      (req, res, ctx) => {
-        res(ctx.status(500));
-      }
+      (req, res, ctx) => res(ctx.status(500))
     )
   );
 
-  render(
-    <BrowserRouter>
-      <EmployerHome />
-    </BrowserRouter>
-  );
+  render(<EmployerHome />);
 
   const errorMessage = await screen.findByText(/Could not fetch employers/i);
   expect(errorMessage).toBeInTheDocument();
